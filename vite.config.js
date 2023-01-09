3445019c0b3b97b31ae66202856ef999b30a1f90
Renat Galin <rensite@yandex.ru>
2023-01-09 13:55:17 +0500

    adding lib

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from 'url'
 
+import { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 
 export default ({ mode }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
@@ -19,6 +21,27 @@ export default ({ mode }) => {
           additionalData: `@import "@/assets/styles/_vars.scss";`
         }
       }
+    },
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: 'lib/main.js',
+        name: 'SPA Template',
+        // the proper extensions will be added
+        fileName: 'spa-template',
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      },
     },
   })
 }
